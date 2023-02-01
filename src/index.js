dd08b1bd138e75c149981d9083b3df795dee6974
TetianaSosiedka <tasha.sosedka@gmail.com>
2023-02-01 12:03:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
@@ -7,8 +8,10 @@ import { theme } from './constants/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter basename="cozy-space-front">
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
