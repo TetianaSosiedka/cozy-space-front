cb69ca3faaea3d54ccb0afb76636a621cd9e8e5c
TetianaSosiedka <tasha.sosedka@gmail.com>
2023-02-01 11:38:03 +0200

    chenge name repositiry

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="test">
+    <BrowserRouter basename="cozy-space-front">
       <App />
     </BrowserRouter>
   </React.StrictMode>
