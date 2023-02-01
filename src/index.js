0f148ffc9c3e0442c8c46f37ca0273234538a184
TetianaSosiedka <tasha.sosedka@gmail.com>
2023-02-01 11:13:00 +0200

    fixing config

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-05-movies">
+    <BrowserRouter basename="test">
       <App />
     </BrowserRouter>
   </React.StrictMode>
