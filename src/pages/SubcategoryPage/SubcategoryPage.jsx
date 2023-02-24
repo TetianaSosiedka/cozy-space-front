b1af4ec4cd6a35ef64ac3a58429532dabc00d403
TetianaSosiedka <tasha.sosedka@gmail.com>
2023-02-24 10:54:06 +0200

    added question list

--- src/pages/SubcategoryPage/SubcategoryPage.jsx
+++ src/pages/SubcategoryPage/SubcategoryPage.jsx
@@ -4,10 +4,19 @@ import Container from 'components/Container/Container';
 import ProductItem from 'components/ProductCard/ProductCard';
 import Footer from 'components/Footer/Footer';
 import ContainerItems from 'components/ContainerItems/ContainerItems';
+//import { questions } from 'constants/questions';
+
+import {
+  Appeal,
+  Checklist,
+  Question,
+  QuestionName,
+  InputWrap,
+} from './SubcategoryPage.styled';
 
 const SubcategoryPage = () => {
-  const { category = 'brends', brend = '' } = useParams();
-  console.log(category, brend);
+  const { category = '', subcategory = '' } = useParams();
+  console.log(category, subcategory);
   const items = [
     <ProductItem key="1" />,
     <ProductItem key="2" />,
@@ -21,7 +30,53 @@ const SubcategoryPage = () => {
   return (
     <>
       <Container>
-        <>Subcategory</>
+        <Appeal>
+          Для індивідуального підбору засобу, дайте відповіді на декілька
+          питань:
+        </Appeal>
+        <Checklist>
+          <Question>
+            <QuestionName>Засіб має:</QuestionName>
+            <InputWrap>
+              <input type="radio" name="tone-do" value="moisten" />{' '}
+              <span>зволожувати</span>
+            </InputWrap>
+            <InputWrap>
+              <input type="radio" name="tone-do" value="matt" />
+              <span> матувати</span>
+            </InputWrap>
+          </Question>
+          <Question>
+            <QuestionName>Текстура:</QuestionName>
+            <InputWrap>
+              <input type="radio" name="tone-texture" value="light" />{' '}
+              <span>легка</span>
+            </InputWrap>
+            <InputWrap>
+              <input type="radio" name="tone-texture" value="dense" />{' '}
+              <span>щільна</span>
+            </InputWrap>
+          </Question>
+          <Question>
+            <QuestionName>Відтінок вашої шкіри:</QuestionName>
+            <InputWrap>
+              <input type="radio" name="tone-tinct" value="pale" />{' '}
+              <span>бліда</span>
+            </InputWrap>
+            <InputWrap>
+              <input type="radio" name="tone-tinct" value="fair" />{' '}
+              <span>світла</span>
+            </InputWrap>
+            <InputWrap>
+              <input type="radio" name="tone-tinct" value="natural" />{' '}
+              <span>натуральна</span>
+            </InputWrap>
+            <InputWrap>
+              <input type="radio" name="tone-tinct" value="tanned" />{' '}
+              <span>засмагла</span>
+            </InputWrap>
+          </Question>
+        </Checklist>
         <ContainerItems items={items} />
       </Container>
       <Footer />
--- src/pages/SubcategoryPage/SubcategoryPage.styled.jsx
+++ src/pages/SubcategoryPage/SubcategoryPage.styled.jsx
@@ -1,19 +1,81 @@
 import styled from 'styled-components';
+import { UnderlineAfter } from 'components/App/App.styled';
 
-// export const Main = styled.main`
-//   display: flex;
-//   flex-wrap: wrap;
-//   padding-top: 40px;
-//   padding-bottom: 60px;
-// `;
+export const Appeal = styled.span`
+  display: inline-block;
+  margin-top: 40px;
+  margin-bottom: 10px;
+  font-size: 25px;
+  font-family: ${props => props.theme.fonts.headers};
+  color: ${props => props.theme.colors.accentActive};
+  @media ${props => props.theme.screens.tablet} {
+    font-size: 30px;
+  }
+`;
 
-// export const ProductWrap = styled.div`
-//   margin-bottom: 15px;
-//   width: 50%;
-//   @media ${props => props.theme.screens.tablet} {
-//     flex-basis: calc((100%) / 3);
-//   }
-//   @media ${props => props.theme.screens.desktop} {
-//     flex-basis: calc((100%) / 4);
-//   }
-// `;
+export const Checklist = styled.div`
+  margin-top: 20px;
+  padding-bottom: 30px;
+  border-top: solid 2px ${props => props.theme.colors.accent};
+  border-bottom: solid 2px ${props => props.theme.colors.accent};
+`;
+
+export const Question = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  font-family: ${props => props.theme.fonts.body};
+`;
+
+export const QuestionName = styled.span`
+  display: flex;
+  width: 100%;
+  margin-top: 30px;
+  margin-bottom: 10px;
+  font-family: ${props => props.theme.fonts.headers};
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 16px;
+`;
+
+export const InputWrap = styled.div`
+  transition: color 0.25s ${props => props.theme.action.timingFunction};
+  :hover {
+    :hover span {
+      color: ${props => props.theme.colors.accent};
+      transition: color 0.25s ${props => props.theme.action.timingFunction};
+    }
+    span::after {
+      transform: scaleX(1);
+      transform-origin: bottom left;
+      transition: transform 0.5s ease-out;
+    }
+  }
+
+  input[type='radio'] {
+    margin-right: 5px;
+    margin-left: 30px;
+    accent-color: ${props => props.theme.colors.accent};
+    :checked + span {
+      color: ${props => props.theme.colors.accent};
+    }
+    :checked + span::after {
+      transform: scaleX(1);
+      transform-origin: bottom left;
+      transition: transform 0.5s ease-out;
+    }
+  }
+
+  span {
+    position: relative;
+    display: inline-block;
+    font-size: 16px;
+    ::after {
+      ${UnderlineAfter};
+      background-color: ${props => props.theme.colors.accent};
+    }
+
+    @media ${props => props.theme.screens.tablet} {
+      font-size: 18px;
+    }
+  }
+`;
